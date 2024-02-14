894dabafd1a27048bce70cef0dd7ca01936929ad
ahsansamad <ingeniouslogo@gmail.com>
2024-02-15 00:18:28 +0500

    doing-practice-js

--- app.js
+++ app.js
@@ -120,6 +120,39 @@
 // if(fuel <= 0.25){
 //   alert("“Please refill the fuel in your car”")
 // }else{alert("dont Worry and go on")}
+
+// var a = 4;
+// if (++a === 5){
+//   alert("given condition for variable a is true");
+//   }
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }else{alert("given condition for variable b is false");
+// }
+
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals " + totalCost);
+// }
+if("car" < "cat"){
+  alert("car is smaller than cat");
+  }
 // ==============assigment-chaper-9-10================
 
 // var input = document.getElementById("inpu");
