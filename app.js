6a1d43e701a98aafc3d1fd4e80649c6820ce4e19
ahsansamad <ingeniouslogo@gmail.com>
2024-09-04 19:46:57 +0500

    repeate Practice

--- app.js
+++ app.js
@@ -1161,54 +1161,65 @@
 //       price : 3500000
 //    }
 // }
-var carName = document.getElementById('carName')
-var carModel = document.getElementById('carModel')
+// var carName = document.getElementById('carName')
+// var carModel = document.getElementById('carModel')
 
-var cars = {
-   honda : {
-         civic : {
-            varient : "Civic Vti Oriel",
-            model : "2023",
-            price : 4000000
-         },
-         city : {
-            varient : "City Aspire",
-            model : "2022",
-            price : 3500000
-         }
-      },
+// var cars = {
+//    honda : {
+//          civic : {
+//             varient : "Civic Vti Oriel",
+//             model : "2023",
+//             price : 4000000
+//          },
+//          city : {
+//             varient : "City Aspire",
+//             model : "2022",
+//             price : 3500000
+//          }
+//       },
       
-      toyota : {
-         corolla : {
-            varient : "altis Vvti ",
-            model : "2023",
-            price : 4000000
-         },
-         fortuner : {
-            varient : "Sigma",
-            model : "2022",
-            price : 3500000
-         }
-      }
+//       toyota : {
+//          corolla : {
+//             varient : "altis Vvti ",
+//             model : "2023",
+//             price : 4000000
+//          },
+//          fortuner : {
+//             varient : "Sigma",
+//             model : "2022",
+//             price : 3500000
+//          }
+//       }
       
-}
-for ( var key in cars){
-   // console.log(key)
-carName.innerHTML += `<option  value="${key}"> ${key}</option>`
+// }
+
+// for(var key in cars){
+//    // console.log(cars[key])
+//    for(var key2 in cars[key]){
+//       // console.log(cars[key][key2])
+//       for(var key3 in cars[key][key2]){
+//          console.log(cars[key][key2])
+//       }
+//    }
+// }
+
+// for ( var key in cars){
+//    // console.log(key)
+// carName.innerHTML += `<option  value="${key}"> ${key}</option>`
  
-}
-function getVarient(){
-   // console.log(event.target.value)
-   var selectedManufacture = event.target.value
-   // console.log(selectedManufacture)
-   carModel.innerHTML =`<option  value=""> Varient</option>`
-   if(selectedManufacture){
-      var models = cars[selectedManufacture]
-   }
-   for(var modelName in cars[selectedManufacture]){
-      carModel.innerHTML += `<option  value=""> ${modelName}</option>`
-      console.log(modelName)
-   }
+// }
+// function getVarient(){
+//    // console.log(event.target.value)
+//    var selectedManufacture = event.target.value
+//    // console.log(selectedManufacture)
+//    carModel.innerHTML =`<option  value=""> Varient</option>`
+//    if(selectedManufacture){
+//       var models = cars[selectedManufacture]
+//    }
+//    for(var modelName in cars[selectedManufacture]){
+//       carModel.innerHTML += `<option  value=""> ${modelName}</option>`
+//       console.log(modelName)
+//    }
    // for (var key1 in cars[key]){
    //    // console.log(key1)
       
@@ -1217,4 +1228,27 @@ function getVarient(){
    //    //    console.log("corolla")
    //    // }
    // }
+// }
+
+
+
+
+
+// ============================================
+// ========================================
+
+//                        August 2024 Practice
+// =================================================
+// ==================================================
+// ============================================
+
+var count = 0
+function abc(){
+   for(i = 0 ; i < 5 ; i++){
+      count++
+      console.log(count)
+   }
 }
+abc()
+abc()
+abc()
